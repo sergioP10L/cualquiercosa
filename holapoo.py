2dc1d74ac099838d1ec9d842996dc3294a373a6a
sergioP10L <sleonardopinzon@ucuncinamarca.edu.co>
2025-04-07 11:28:40 -0500

    peluquerot

---
+++ holapoo.py
@@ -0,0 +1,10 @@
+class saludo:
+
+    def  __init__ (self,mensaje):
+      self.mesaje = mensaje
+
+      def mostrarmensaj(self):
+                print(self.mensaje)
+    
+    saludo = saludo ("hola mundo")
+    saludo.mostrarmensaje
